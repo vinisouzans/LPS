ac2f84827f92f44b448f2edabb6941c7ab7074ea
Vinicius Souza <vini_souza1992@hotmail.com>
2025-08-14 15:23:00 -0300

    Ajuste de estoque ao excluir e editar uma venda

---
+++ LPS/DTOs/Venda/VendaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LPS.DTOs.Venda
+{
+    public class VendaDTO
+    {
+        public decimal Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public int ProdutoId { get; set; }
+        public int EstoqueId { get; set; }
+    }
+}
---
+++ LPS/Migrations/20250814175831_CriandoTabelaVenda.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LPS.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoTabelaVenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataVenda = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Quantidade = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ValorUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    EstoqueId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Estoques_EstoqueId",
+                        column: x => x.EstoqueId,
+                        principalTable: "Estoques",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_EstoqueId",
+                table: "Vendas",
+                column: "EstoqueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_ProdutoId",
+                table: "Vendas",
+                column: "ProdutoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vendas");
+        }
+    }
+}
